ed758b29418b98d41affff8f8c272077f5854a28
MarinhoCM <marinhoc384@gmail.com>
2023-09-27 11:20:29 -0300

    :sparkles: feat: inserindo cores ao output do terminal

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 /*Elabore um programa em C/C++ que calcule o que deve ser pago por um produto,
-considerando o preço normal de etiqueta e a escolha da condição de pagamento.
-Utilize os códigos da tabela a seguir para ler qual a condição de pagamento 
+considerando o pre�o normal de etiqueta e a escolha da condição de pagamento.
+Utilize os códigos da tabela a seguir para ler qual a condi��o de pagamento 
 escolhida e efetuar o cálculo adequado*/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
-#include <locale>
+#include <locale.h>
+#include <windows.h>
+#include <string.h>
 
-#define BLK "\e[0;30m"
 #define RED "\e[0;31m"
 #define GRN "\e[0;32m"
 #define YEL "\e[0;33m"
-#define BLU "\e[0;34m"
-#define MAG "\e[0;35m"
-#define CYN "\e[0;36m"
-#define WHT "\e[0;37m"
 #define END "\e[0;0m"
 
 #define MESSAGE_RED(X) printf("%s %s %s",RED,X,END)
+#define MESSAGE_GREEN(X) printf("%s %s %s",GRN,X,END)
+#define MESSAGE_YELLOW(X) printf("%s %s %s",GRN,X,END)
+#define INFO_MESSAGE(X,Y) printf("%s %s %s %s ",YEL,X,Y,END)
 
 float product_price(void){
 	float price;
-	printf("Digite o valor do seu produto: ");
+	MESSAGE_GREEN("Digite o valor do seu produto: ");
 	scanf("%f", &price);
 	if(price <= 0.0){
-		printf("Digite um valor válido!\n");
+		MESSAGE_RED("Digite um valor v�lido!\n");
 		product_price();
 		return 0.0;
 	} else {
@@ -34,13 +35,13 @@ float product_price(void){
 
 int menu(void){
 	int option;
-	printf("Selecione a forma de pagamento:\n");
-	printf("\t1 - A vista em dinheiro ou pix, recebe 10%% de desconto.\n");
-	printf("\t2 - A vista no cartão de credito, recebe 5%% de desconto.\n");
-	printf("\t3 - Em duas vezes, preço normal de etiqueta sem juros.\n");
-	printf("\t4 - Em três vezes, preço normal de etiqueta mais juros de 10%%.\n");
-	printf("\t5 - Sair.\n");
-	printf("\t-----> ");
+	MESSAGE_GREEN("Selecione a forma de pagamento:\n");
+	MESSAGE_GREEN("\t1 - A vista em dinheiro ou pix, recebe 10%% de desconto.\n");
+	MESSAGE_GREEN("\t2 - A vista no cart�o de credito, recebe 5%% de desconto.\n");
+	MESSAGE_GREEN("\t3 - Em duas vezes, pre�o normal de etiqueta sem juros.\n");
+	MESSAGE_GREEN("\t4 - Em tr�s vezes, pre�o normal de etiqueta mais juros de 10%%.\n");
+	MESSAGE_GREEN("\t5 - Sair.\n");
+	MESSAGE_GREEN("\t-----> ");
 	scanf("%i", &option);	
 	return option;
 }
@@ -51,20 +52,20 @@ float calculator(int option, float price){
 		case 1:
 			discount = price * 0.1;
 			total = price - discount;
-			printf("O valor do seu produto é R$ %.2f", total);
+			INFO_MESSAGE("O valor do seu produto � R$ %.2f", total);
 			break;
 		case 2:
 			discount = price * 0.05;
 			total = price - discount;
-			printf("O valor do seu produto é R$ %.2f", total);
+			INFO_MESSAGE("O valor do seu produto � R$ %.2f", total);
 			break;
 		case 3:
 			total = price / 2;
-			printf("O seu produto terá duas parcelas de R$ %.2f", total);
+			INFO_MESSAGE("O seu produto ter� duas parcelas de R$ %.2f", total);
 			break;
 		case 4:
 			total = price / 3;
-			printf("O seu produto terá três parcelas de R$ %.2f", total);
+			INFO_MESSAGE("O seu produto ter� tr�s parcelas de R$ %.2f", total);
 			break;
 	}
 }
@@ -75,8 +76,8 @@ int main(void){
 	int option = menu();
 	if (option == 5){
 		return 0;
-	} else if (option == 0 || option >= 6){
-		printf("Digite uma opção válida!\n");
+	} else if (option < 0 || option == 0 ||option >= 6){
+		MESSAGE_YELLOW("Digite uma op��o v�lida!\n");
 		int option = menu();
 	}
 	calculator(option, price);
