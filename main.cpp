de5b9a8f379cd88ecaa3d8fe81ced740bd26dda7
MarinhoCM <marinhoc384@gmail.com>
2023-09-27 11:24:11 -0300

    :sparkles: feat: aplicando correções ao texto

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*Elabore um programa em C/C++ que calcule o que deve ser pago por um produto,
-considerando o pre�o normal de etiqueta e a escolha da condição de pagamento.
-Utilize os códigos da tabela a seguir para ler qual a condi��o de pagamento 
+considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+Utilize os códigos da tabela a seguir para ler qual a condição de pagamento 
 escolhida e efetuar o cálculo adequado*/
 
 #include <stdio.h>
