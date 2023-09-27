e121920b445f6afabf001576ef49b96a5b348875
MarinhoCM <marinhoc384@gmail.com>
2023-09-27 09:04:36 -0300

    :tada: init: initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/*Elabore um programa em C/C++ que calcule o que deve ser pago por um produto,
+considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+Utilize os códigos da tabela a seguir para ler qual a condição de pagamento 
+escolhida e efetuar o cálculo adequado*/
+
+
+int main(void){
+    return 0;
+}
